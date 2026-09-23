cd231330c84ee393e15fc922bf8fec9a1eca8f3f
Reihihihiha <111255618+Maxshiel@users.noreply.github.com>
2026-09-22 21:41:13 -0300

    Add files via upload

---
+++ redesocial/cookies.js
@@ -0,0 +1,35 @@
+function criarCookie(nome, valor, expira){
+    var dtExpira = "expires="+expira;
+    document.cookie = nome + "=" + valor + ";" + dtExpira;
+    //cookie final = nomeUsuario=nome; expires= Tue, 01 Jan 2115 120000 UTC 
+}
+
+function lerCookie(nome){
+    var vnome = nome + "=";
+    //vnome - nomeUsuario=
+    var ca = document.cookie.split(';');
+    //ca - nomeUsuario=nome
+    for(var i=0; i<ca.length; i++){
+        var c = ca[i];
+        while(c.charAt(0)==' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(vnome) == 0)
+            return c.substring(vnome.length,c.length);
+    }
+    return "";
+}
+
+function verificarCookie(){
+    var username=lerCookie("nomeUsuario");
+    if(username != ""){
+        alert("Bem vindo novamente" + username);
+    }else{
+        username =  prompt("Digite seu nome");
+        if (username != '' && username != null){
+            criarCookie('nomeUsuario', username,'Tue, 01 Jan 2115 120000 UTC');
+        }
+    }
+}
+
+verificarCookie();
